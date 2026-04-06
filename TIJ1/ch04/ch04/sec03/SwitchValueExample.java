007994891fddf10f91dda2fe920b5956e6700945
kyr <par4mit4@gmail.com>
2026-04-07 01:43:47 +0900

    2026.04.07 commit

---
+++ TIJ1/ch04/ch04/sec03/SwitchValueExample.java
@@ -0,0 +1,29 @@
+package ch04.sec03;
+
+public class SwitchValueExample {
+
+	public static void main(String[] args) {
+		String grade = "B";
+		
+		// java 11 이전
+		int score1 = 0;
+		switch(grade) {
+		case "A": score1 = 100 ; break;
+		case "B": score1 = 100-20 ; break;
+		default: score1 = 60;
+		}
+		System.out.println("Score1: " + score1 ); 
+		
+		// java 13 이후
+		int score2 = switch(grade) {
+		case "A" -> 100;
+		case "B" -> {
+			int result = 100 - 20;
+			yield result;
+		}
+		default -> 60;
+		};
+		System.out.println("Score2: " + score2);
+	}
+
+}
